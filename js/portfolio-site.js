4fc9381d878d6f1141d48fb1ef758d869437b4de
Isaac Diaz <itdiaz2014@gmail.com>
2026-02-24 23:08:21 -0800

    fix iframe src and load iframe api to enable player events

--- js/portfolio-site.js
+++ js/portfolio-site.js
@@ -11,18 +11,15 @@ hamMenuIcon.addEventListener("click", () => {
     navMenu.classList.toggle("open");
 });
 
+// Load the YouTube IFrame Player API code asynchronously to control iframe youtube videos in js
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var player;
 function onYouTubeIFrameAPIReady() {
     player = new YT.Player("player", {
-        videoId: 'MhCSIy0RpQ8',
-        playerVars: {
-            'playsinline': 1,
-            'controls': 0,
-            'origin': "https://www.youtube.com",
-            'autoplay': 1,
-            'rel': 0,
-            'disablekb': 1
-        },
         events: {
             'onReady': onPlayerReady
         }
@@ -30,12 +27,15 @@ function onYouTubeIFrameAPIReady() {
 };
 
 function onPlayerReady(event) {
+    // Need to add border in CSS to see this color
     document.getElementById('player').style.borderColor = '#FF6D00';
     event.target.playVideo();
 }
 
 const testBtn = document.querySelector(".test");
 testBtn.addEventListener("click", () => {
+    // TODO: Need to set proper url parameters to disable keyboard, autoplay, etc.
+    // TODO: Need to set proper origin domain where js code comes from (I think github pages)
     const newURL = "https://www.youtube.com/embed/LDJrs22kqb0";
     document.getElementById('player').setAttribute('src', newURL);
 });

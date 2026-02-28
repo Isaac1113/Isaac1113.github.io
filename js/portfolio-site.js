84697179d2a210235e5362245360f923a5cee40c
Isaac Diaz <itdiaz2014@gmail.com>
2026-02-27 21:04:37 -0800

    auto populate the quick view gallery with project cover images

--- js/portfolio-site.js
+++ js/portfolio-site.js
@@ -1,17 +1,27 @@
+const galleryItems = [
+    {videoID: "MhCSIy0RpQ8", title: "Labyrinth of Pushdown Palindrome Level", cover: "img/PDAGame/CoverImage.png"},
+    {videoID: "LDJrs22kqb0", title: "Slash Gameplay Tech Demo", cover: "img/Slash/CoverImage.png"},
+    {videoID: "TIjEzQFmSDo", title: "Unity Project Beatshot Gameplay", cover: "img/Beatshot/CoverImage.png"},
+    {videoID: "sMCxA6FZ1Fc", title: "Impact Miner Full Gameplay", cover: "img/ImpactMiner/CoverImage.png"},
+    {videoID: "3Us5JCDH3ag", title: "Ray Tracer Final Animation", cover: "img/RayTracer/CoverImage.png"}
+];
+
+const quickViewGallery = document.querySelector(".carousel-gallery");
 const hamMenuIcon = document.querySelector(".hamburger-nav");
 
+/* Add event listener to hamburger menu to transition to the open menu */
 hamMenuIcon.addEventListener("click", () => {
-    // Animate the hamburger menu to be an close button
+    // Animate the hamburger menu to be a close button
     for (const child of hamMenuIcon.children) {
         child.classList.toggle("open");
     }
 
-    // Animate the nav menu appearing and comint into view for mobile
+    // Animate the nav menu appearing and come into view for mobile
     const navMenu = document.getElementById("nav-bar");
     navMenu.classList.toggle("open");
 });
 
-// Load the YouTube IFrame Player API code asynchronously to control iframe youtube videos in js
+/* Load the YouTube IFrame Player API code asynchronously to control iframe youtube videos in js */
 var tag = document.createElement('script');
 tag.src = 'https://www.youtube.com/iframe_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -39,3 +49,25 @@ testBtn.addEventListener("click", () => {
     const newURL = "https://www.youtube.com/embed/LDJrs22kqb0";
     document.getElementById('player').setAttribute('src', newURL);
 });
+
+/* Create the img elements from the galleyItems and populate the quick view gallery carousel */
+// Add an empty carousel-slide div to the front of the gallery carousel
+const frontSlide = document.createElement("div");
+frontSlide.classList.add("carousel-slide");
+quickViewGallery.appendChild(frontSlide);
+for (const item of galleryItems) {
+    const slide = document.createElement("div");
+    slide.classList.add("carousel-slide");
+
+    const slideImg = document.createElement("img");
+    slideImg.setAttribute("src", item.cover);
+    slideImg.setAttribute("alt", item.title + "cover image");
+
+    slide.appendChild(slideImg);
+
+    quickViewGallery.appendChild(slide);
+}
+// Add an empty carousel-slide div to the end of the gallery carousel
+const endSlide = document.createElement("div");
+endSlide.classList.add("carousel-slide");
+quickViewGallery.appendChild(endSlide);

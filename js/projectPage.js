66a4d17bfd2b8bb066d5f89a7e8df557474a6a7c
Isaac Diaz <itdiaz2014@gmail.com>
2026-09-15 16:25:35 -0700

    testing animation leave on main container

--- js/projectPage.js
+++ js/projectPage.js
@@ -3,6 +3,15 @@ const projSearchParams = new URLSearchParams(paramsString);
 
 const projectInfoContainer = document.querySelector(".project-info-container");
 
+const nextButton = document.querySelector(".next");
+
 const pProj = document.querySelector("main > p");
 pProj.textContent += projSearchParams.get("projNum");
 
+nextButton.addEventListener("click", (event) => {
+    projectInfoContainer.classList.toggle("leave");
+});
+
+projectInfoContainer.addEventListener("animationend", (event) => {
+    projectInfoContainer.classList.toggle("leave");
+});

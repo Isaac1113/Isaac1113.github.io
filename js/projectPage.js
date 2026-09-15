f1d252fc46d3a0a4a051bc5edd4760358fa94890
Isaac Diaz <itdiaz2014@gmail.com>
2026-09-15 15:56:28 -0700

    add layout and dummy info to project page

--- js/projectPage.js
+++ js/projectPage.js
@@ -1,5 +1,8 @@
 const paramsString = window.location.search;
 const projSearchParams = new URLSearchParams(paramsString);
 
+const projectInfoContainer = document.querySelector(".project-info-container");
+
 const pProj = document.querySelector("main > p");
 pProj.textContent += projSearchParams.get("projNum");
+

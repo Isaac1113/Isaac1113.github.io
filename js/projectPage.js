46ae475ff67cb4ef1d7aec828a45148bb2fdc943
Isaac Diaz <itdiaz2014@gmail.com>
2026-09-18 20:17:46 -0700

    style file ul, populate project page with data

--- js/projectPage.js
+++ js/projectPage.js
@@ -6,7 +6,7 @@ const projectBaseURL = "https://raw.githubusercontent.com/Isaac1113/Isaac1113.gi
 const projectListURL = "https://raw.githubusercontent.com/Isaac1113/Isaac1113.github.io/refs/heads/main/data/projectDataList.json";
 let projectDataList;
 let projectData;
-let numFiles = 0;
+let currentProjectFileIdx = 0;
 
 const projectInfoContainer = document.querySelector(".project-info-container");
 
@@ -15,8 +15,36 @@ const nextButton = document.querySelector(".next");
 const pProj = document.querySelector("main > p");
 pProj.textContent += currentProjectNum;
 
-const projectFileList = document.querySelector(".project-file-list");
+const pTitle = document.querySelector(".p-title");
+const pCatAndTech = document.querySelector(".p-cat-and-tech");
+const pTime = document.querySelector(".p-time");
+const pDescription = document.querySelector(".p-description");
+const pContributions = document.querySelector(".p-contributions");
 
+const projectFileList = document.querySelector(".project-file-list");
+const fileTitle = document.querySelector(".file-title");
+
+
+/* Load the YouTube IFrame Player API code asynchronously to control iframe youtube videos in js */
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+        events: {
+            'onReady': onPlayerReady
+        }
+    })
+};
+
+function onPlayerReady(event) {
+    // Need to add border in CSS to see this color
+    document.getElementById('player').style.borderColor = '#FF6D00';
+    // event.target.playVideo();
+}
 
 /* initialization to get list of projects in specific order */
 async function fetchProjectList() {
@@ -35,9 +63,50 @@ async function pageInitialization() {
     await fetchProjectList();
 
     await fetchProjectFiles(currentProjectNum);
+
+    updatePageData();
 }
 
 pageInitialization();
+
+/* populate page with data from project data */
+function updatePageData() {
+    pTitle.textContent = projectData.name;
+    pCatAndTech.textContent = `${projectData.category} (${projectData.technology})`;
+    pTime.textContent = projectData.time;
+    pDescription.textContent = projectData.description;
+    pContributions.textContent = projectData.contribution;
+
+    // load all project videos and images into unordered list
+    for (let i = 0; i < projectData.files.length; i++) {
+        let elem;
+
+        if (projectData.files[i].type === "video") {
+            elem = document.createElement("video");
+            elem.controls = true;
+        }
+        else {
+            elem = document.createElement("img");
+        }
+
+        elem.src = projectData.files[i].source;
+
+        projectFileList.appendChild(elem);
+    }
+
+    updateFileData(0);
+
+    // load youtube iframe video at bottom of page
+    const projectIframeURL = `https://www.youtube.com/embed/${projectData.viedoID}?enablejsapi=1&mute=1&rel=0`;
+    document.getElementById("player").contentWindow.location.replace(projectIframeURL);
+}
+
+/* update the specific file info that is displayed when a file image/video is clicked */
+function updateFileData(fileIdx) {
+    fileTitle.textContent = projectData.files[fileIdx].title;
+
+    // TODO: set the text of all the other file specific elements
+}
 
 /* testing leaving and entering animation of data content */
 nextButton.addEventListener("click", (event) => {
